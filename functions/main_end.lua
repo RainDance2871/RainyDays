38b1c62c4348cb66221fe9c5f6cd1ec49e6bb014
RainDance2871 <erik.nemmers@gmail.com>
2026-09-15 01:07:16 +0200

    Delete functions directory

--- functions/main_end.lua
+++
@@ -1,7 +0,0 @@
-function RainyDays.generate_main_end_counter(text)
-  return {
-    { n = G.UIT.T, config = { text = '[', colour = G.C.UI.TEXT_INACTIVE, scale = 0.32 }},
-    { n = G.UIT.T, config = { text = text, colour = G.C.UI.TEXT_INACTIVE, scale = 0.32 }},
-    { n = G.UIT.T, config = { text = ']', colour = G.C.UI.TEXT_INACTIVE, scale = 0.32 }}
-  }
-end
